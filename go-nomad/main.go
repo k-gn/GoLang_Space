c8d4feb22db626248d2a0758ee4096a87ac1c181
k-gn <rlarbska97@gmail.com>
2022-11-30 09:44:02 +0900

    feat: 1-2 variables and constants

--- go-nomad/main.go
+++ go-nomad/main.go
@@ -1,19 +1,37 @@
 package main
 
-// main.go file 은 프로젝트가 컴파일을 해야할 경우 필요하다. (진입점)
-// 이 외의 파일명은 컴파일 되지 않는다. (ex. learning.go) 기능의 묶음 혹은 다른 사람들이 사용할 것들
-
-import (
-	"fmt" // go 가 가고있는 패키지들 중 하나
-
-	"github.com/k-gn/learngo/go-nomad/something"
-)
+/*
+	main.go file 은 프로젝트가 컴파일을 해야할 경우 필요하다. (진입점)
+	이 외의 파일명은 컴파일 되지 않는다. (ex. learning.go) 기능의 묶음 혹은 다른 사람들이 사용할 것들
+	go 는 다양한 패키지들을 제공하고 있다.
+*/
 
 func main() {
+	/*
+		function 을 export 대문자로 시작
 
-	fmt.Println("Hello Go!!")
+			something.SayHello() (O)
+			something.sayBye()	(X)
+	*/
 
-	// function 을 export 대문자로 시작
-	something.SayHello()
-	// something.sayBye()
+	/*
+		Go 는 타입 언어이다.
+
+		- 상수 : const
+			const name string = "gyunam"
+
+		- 변수 : var
+			var name string = "gyunam"
+			name = "donggyu"
+
+		위 방식을 축약할 수 있다. (이 때 Go 가 자동으로 타입을 추론해준다.)
+			name := "gyunam"
+
+		처음 선언된 타입에 의존한다.
+			name = false (X)
+
+		함수 밖에선 축약할 수 없다.
+			var flag bool = false
+	*/
+
 }
